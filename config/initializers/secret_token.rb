df432580a08ec7fdce2cc753616138db9dfc45b5
alonso523 <alonso523@outlook.com>
2012-10-16 09:27:31 -0600

    subir archivos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebAlonsoTp2::Application.config.secret_token = 'af544c6cfde77a7cb87f1494b3285bf560492777e275495833f3dccc312e2781cc091384270a18b15a317acc032c98d2fc2a211b561dcabc372f8156e28e0d3b'
